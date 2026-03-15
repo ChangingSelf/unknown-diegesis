f7a27f5ac7180f79120a0e6623992edb9a297277
ChangingSelf <ChangingSelf@163.com>
2026-03-15 15:03:11 +0800

    refactor: 移除未使用的文件和依赖
    - 删除 src/extensions/index.ts (桶文件)
    - 删除 src/utils/IntegrityChecker.ts (未使用)
    - 删除 src/utils/performanceTest.ts (未使用)
    - 移除未使用依赖: @tiptap/extension-document, @tiptap/extension-drag-handle, @tiptap/extension-node-range

--- src/extensions/index.ts
+++
@@ -1,5 +0,0 @@
-export { DiceBlockExtension } from './nodes/DiceBlockExtension';
-export { ImageBlockExtension } from './nodes/ImageBlockExtension';
-export { LayoutRowExtension } from './nodes/LayoutRowExtension';
-export { LayoutColumnExtension } from './nodes/LayoutColumnExtension';
-export { PasteImageExtension } from './nodes/PasteImageExtension';
